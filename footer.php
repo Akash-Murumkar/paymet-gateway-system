1b1fc6dc7b91e003077383aa537848adb419c2d5
Akash-Murumkar <89271707+Akash-Murumkar@users.noreply.github.com>
2021-08-21 01:45:07 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+  <style>
+        .footer{
+            background-color: black;
+            height: auto;
+            width: 100%;
+            margin-top: 30px;
+            padding:5px;
+            position:fixed;
+            bottom:0;
+        }
+        .footer p{
+            margin:20px;
+            color: white;
+            text-align: center;
+            font-size: 18px;
+        }
+        .footer p a{
+            color: white;
+        }
+        .footer p a:hover{
+            color: yellow;
+            text-decoration: none;
+        }
+        @media(max-width : 600px)
+        {
+            .footer p{
+             font-size: 15px;
+            }
+        }
+  </style>
+</head>
+<body>  
+   <!-- Footer start -->
+   <div class="footer"> 
+           <p><b>Akash Murumkar</b></a> || The Sparks Foundation || GRIP </p>
+   </div>
+   <!-- Footer end -->
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,24 @@
+<html>
+    <body>
+         <!-- navbar start -->
+            <div class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" type="button" data-target="#colNavBar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="index.php" class="navbar-brand">Spark Foundation</a> 
+                </div>
+                <div class="collapse navbar-collapse" id="colNavBar">
+                    <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="form.php">DONATE</a></li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+    <!-- navbar end  -->
+    </body>
+</html>
